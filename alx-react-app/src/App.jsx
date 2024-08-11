b4ba8ffffbf857a2f54349ea95aa1fe3ae4ff02f
Abrish <abrish4ever2015@gmail.com>
2024-08-11 16:42:20 +0300

    Add multiple components to App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 import WelcomeMessage from './WelcomeMessage'
 
 function App() {
@@ -10,6 +9,9 @@ function App() {
   return (
     <>
       <WelcomeMessage/>
+      <Header/>
+      <MainContent/>
+      <Footer/>
     </>
   )
 }
