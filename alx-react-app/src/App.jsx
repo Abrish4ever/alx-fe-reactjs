ec00877d113b8057bb9bb09b40e386f6166869ac
Abrish <abrish4ever2015@gmail.com>
2024-08-11 16:44:13 +0300

    remove WelcomeMessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,12 @@
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
-import WelcomeMessage from './WelcomeMessage'
 
 function App() {
 
 
   return (
     <>
-      <WelcomeMessage/>
       <Header/>
       <MainContent/>
       <Footer/>
