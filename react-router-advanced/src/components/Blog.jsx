4cb6b4464e42943b9424afb0b4e9720212edfbc6
Abrish <abrish4ever2015@gmail.com>
2024-09-01 21:26:07 +0300

    Add react reactor advanced

---
+++ react-router-advanced/src/components/Blog.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function Blog() {
+  const { postId } = useParams();
+
+  return (
+    <div>
+      <h1>Blog Post</h1>
+      <p>Displaying content for post ID: {postId}</p>
+    </div>
+  );
+}
+
+export default Blog;
