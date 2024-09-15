a0aa8782895b25cd3fcfdc0010e86272177e3cc3
Abrish <abrish4ever2015@gmail.com>
2024-09-15 18:42:31 +0300

    Add some changes

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import RecipeDetail from './components/RecipeDetail'
 
